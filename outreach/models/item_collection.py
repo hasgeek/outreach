b9afc45178831002a79934e8f1ac7018dca99056
Shreyas Satish <shreyas.satish@gmail.com>
2016-10-19 19:01:23 +0530

    cleanup

--- outreach/models/item_collection.py
+++ outreach/models/item_collection.py
@@ -12,14 +12,13 @@ class ItemCollection(BaseScopedNameMixin, db.Model):
     __uuid_primary_key__ = True
     __table_args__ = (db.UniqueConstraint('organization_id', 'name'),)
 
-    description = MarkdownColumn('description', default=u'', nullable=False)
+    description = MarkdownColumn('description', default=u"", nullable=False)
 
-    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'),
+    organization_id = db.Column(None, db.ForeignKey('organization.id'),
         nullable=False)
     organization = db.relationship(Organization,
         backref=db.backref('item_collections', cascade='all, delete-orphan'))
 
     # The currently used field in details is refund_policy (html)
     details = db.Column(JsonDict, server_default='{}', nullable=False)
-
     parent = db.synonym('organization')
